f883db1728e04b76a4704321276f91e74c8da7c3
sabrinothman95 <68713978+sabrinothman95@users.noreply.github.com>
2024-10-24 18:36:27 -0400

    Resolved merge conflicts

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,156 +1,3 @@
-import Recipe from '../models/Recipe.js'; // Correct import
-import { body, validationResult } from 'express-validator';
-
-export const createRecipe = [
-  body('name').notEmpty().withMessage('Name is required'),
-  body('ingredients').notEmpty().withMessage('Ingredients are required'),
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    try {
-      const newRecipe = await Recipe.create(req.body);
-      res.status(201).json(newRecipe);
-    } catch (error) {
-      res.status(400).json({ error: error.message });
-    }
-  }
-];
-
-export const getAllRecipes = async (req, res) => {
-  try {
-    const recipes = await Recipe.findAll();
-    res.status(200).json(recipes);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-export const getRecipeById = async (req, res) => {
-  try {
-    const recipe = await Recipe.findByPk(req.params.id);
-    if (recipe) {
-      res.status(200).json(recipe);
-    } else {
-      res.status(404).json({ error: 'Recipe not found' });
-    }
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-export const updateRecipe = async (req, res) => {
-  try {
-    const [updated] = await Recipe.update(req.body, {
-      where: { recipeID: req.params.id },
-    });
-    if (updated) {
-      const updatedRecipe = await Recipe.findByPk(req.params.id);
-      res.status(200).json(updatedRecipe);
-    } else {
-      res.status(404).json({ error: 'Recipe not found' });
-    }
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-};
-
-export const deleteRecipe = async (req, res) => {
-  try {
-    const deleted = await Recipe.destroy({
-      where: { recipeID: req.params.id },
-    });
-    if (deleted) {
-      res.status(204).send();
-    } else {
-      res.status(404).json({ error: 'Recipe not found' });
-    }
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-export default {
-  createRecipe,
-  getAllRecipes,
-  getRecipeById,
-  updateRecipe,
-  deleteRecipe
-};
-
-const { Recipe } = require('./models'); 
-const { body, validationResult } = require('express-validator'); 
-
-const createRecipe = [
-  body('name').notEmpty().withMessage('Name is required'),
-  body('ingredients').notEmpty().withMessage('Ingredients are required'),
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    try {
-      const newRecipe = await Recipe.create(req.body);
-      res.status(201).json(newRecipe);
-    } catch (error) {
-      res.status(400).json({ error: error.message });
-    }
-  }
-];
-
-const getAllRecipes = async (req, res) => {
-  try {
-    const recipes = await Recipe.findAll();
-    res.status(200).json(recipes);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-const getRecipeById = async (req, res) => {
-  try {
-    const recipe = await Recipe.findByPk(req.params.id);
-    if (recipe) {
-      res.status(200).json(recipe);
-    } else {
-      res.status(404).json({ error: 'Recipe not found' });
-    }
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-const updateRecipe = async (req, res) => {
-  try {
-    const [updated] = await Recipe.update(req.body, {
-      where: { recipeID: req.params.id },
-    });
-    if (updated) {
-      const updatedRecipe = await Recipe.findByPk(req.params.id);
-      res.status(200).json(updatedRecipe);
-    } else {
-      res.status(404).json({ error: 'Recipe not found' });
-    }
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-};
-
-const deleteRecipe = async (req, res) => {
-  try {
-    const deleted = await Recipe.destroy({
-      where: { recipeID: req.params.id },
-    });
-    if (deleted) {
-      res.status(204).send(); 
-    } else {
-      res.status(404).json({ error: 'Recipe not found' });
-    }
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
 const { Recipe } = require('../models'); // Correct path to models directory
 const { body, validationResult } = require('express-validator');
 
@@ -229,6 +76,8 @@ const deleteRecipe = async (req, res) => {
   }
 };
 
+<<<<<<< HEAD
+=======
 module.exports = {
   createRecipe,
   getAllRecipes,
@@ -236,3 +85,4 @@ module.exports = {
   updateRecipe,
   deleteRecipe,
 };
+>>>>>>> origin
