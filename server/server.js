da360f1768a75be51104ad24ae0268248a0a4378
sabrinothman95 <68713978+sabrinothman95@users.noreply.github.com>
2024-10-29 14:39:56 -0400

    conflict res

--- server/server.js
+++ server/server.js
@@ -2,9 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import helmet from 'helmet';
-import cors from 'cors';
-import compression from 'compression';
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
